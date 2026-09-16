228112901e6e4472457d7612b9a126473483d7ba
Giacomo Trezzi <giacomo.trezzi@gmail.com>
2026-09-16 15:49:45 +0200

    url pubblico

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
 export default defineConfig({
-  site: process.env.PUBLIC_SITE ?? 'https://YOUR_GITHUB_USER.github.io',
-  base: process.env.PUBLIC_BASE ?? '/minipc-finder',
+  site: 'https://g3z.github.io',
+  base: '/minipc-finder',
   integrations: [preact()],
 });
